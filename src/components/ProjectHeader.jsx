ffa3f86892bc6634c0e5b1514ca300d3d89a7dd3
jjkool1214 <jaysenstolarz@gmail.com>
2025-03-05 19:44:09 -0500

    Me when I got that fanta in my system

--- src/components/ProjectHeader.jsx
+++ src/components/ProjectHeader.jsx
@@ -1,8 +1,12 @@
 export default function ProjectHeader(props){
     return(
-        <div className="justify-center border-4 rounded-xl relative text-center">
-            <img src={props.filename} className="opacity-50 w-20 h-20" alt={props.title}/>
-            <div className={"text-white-001 top-0 left-0"}>{props.title}</div>
-        </div>
+        <>
+            <div className="justify-center border-4 rounded-xl relative text-center">
+                <button className={"hover:text-blue-003"}>
+                    <img src={props.filename} className="opacity-50" alt={props.title}/>
+                    <h2 className={"text-white-001 top-0 left-0 absolute w-full h-full text-center flex items-center justify-center"}>{props.title}</h2>
+                </button>
+            </div>
+        </>
     )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,9 +1,8 @@
 export default function Resume(props){
     return(
-        <div className={"flex flex-col items-center pt-20"}>
-            <h1 className={"text-6xl text-white-001"}>Education</h1>
-            <div id={"education "}>
-
+        <div className={"flex flex-col items-center pt-5"}>
+            <div id={"education"} className={"flex justify-center items-center w-[50vw] h-[70vh]"}>
+                <embed src="src/assets/documents/resume_03_05_2025.pdf" type="application/pdf" className={"w-full h-full"}/>
             </div>
         </div>
     )
