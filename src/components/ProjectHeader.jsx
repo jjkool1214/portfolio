446140ae2d6aaea3400210b344fbce323b559ed0
jjkool1214 <jaysenstolarz@gmail.com>
2025-03-08 11:32:05 -0500

    pushingggg

--- src/components/ProjectHeader.jsx
+++ src/components/ProjectHeader.jsx
@@ -1,12 +1,10 @@
 export default function ProjectHeader(props){
     return(
-        <>
-            <div className="justify-center border-4 rounded-xl relative text-center">
-                <button className={"hover:text-blue-003"}>
-                    <img src={props.filename} className="opacity-50" alt={props.title}/>
-                    <h2 className={"text-white-001 top-0 left-0 absolute w-full h-full text-center flex items-center justify-center"}>{props.title}</h2>
-                </button>
-            </div>
-        </>
+        <div className="justify-center border-4 rounded-xl relative text-center hover:text-blue-002 mt-5">
+            <button className={""}>
+                <img src={props.filename} className="opacity-50" alt={props.title}/>
+                <h2 className={"hover:text-blue-002 text-white-001 top-0 left-0 absolute w-full h-full text-center flex items-center justify-center"}>{props.title}</h2>
+            </button>
+        </div>
     )
 }
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,6 +4,7 @@ export default function Resume(props){
             <div id={"education"} className={"flex justify-center items-center w-[50vw] h-[70vh]"}>
                 <embed src="src/assets/documents/resume_03_05_2025.pdf" type="application/pdf" className={"w-full h-full"}/>
             </div>
+            <a href={"src/assets/documents/resume_03_05_2025.pdf"} download={"jaysen_stolarz_2025.pdf"} className={"underline text-white-001 text-4xl hover:text-blue-002"}>Download</a>
         </div>
     )
 }
